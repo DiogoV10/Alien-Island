a04bc2b8b14de4ba0f1f07d0b2c600904bf2ffe6
Francisco Andrade <f-infsec03@hotmail.com>
2023-10-17 04:17:46 +0100

    Bodd Levitate upgrade

--- Assets/Scripts/EnemyRelated/Boss1/Boss.cs
+++ Assets/Scripts/EnemyRelated/Boss1/Boss.cs
@@ -5,33 +5,46 @@ using UnityEngine;
 public class Boss : MonoBehaviour
 {
     [SerializeField] private Transform target;
-    public GameObject obj;
-
+    //public GameObject obj;
     [Header("Attributes")]
-    [SerializeField] EnemySO boss;
+
+    [SerializeField] private EnemySO boss;
+    [SerializeField] private List<Objectsprojectiles> objects;
+
     private float throwCountDown = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         if (throwCountDown <= 0f)
         {
-            Throw();
+            ChooseRandomProjectile();
             throwCountDown = 1f / boss.fireRate;
         }
 
         throwCountDown -= Time.deltaTime;
     }
 
-    void Throw()
+    void LevitateAll()
     {
-        Objectsprojectiles objGO = obj.GetComponent<Objectsprojectiles>();
-        if (objGO != null) objGO.Seek(target);
+        //foreach (Objectsprojectiles obj in objects) StartCoroutine(obj.Levitate());
+    }
+
+    void ChooseRandomProjectile()
+    {
+        int random = Random.Range(0, objects.Count);
+        //Objectsprojectiles objGO = obj.GetComponent<Objectsprojectiles>();
+        if (objects != null) 
+        {
+            objects[random].Seek(target); 
+        }  
     }
 
 }
--- Assets/Scripts/EnemyRelated/Boss1/ObjectsProjectiles.cs
+++ Assets/Scripts/EnemyRelated/Boss1/ObjectsProjectiles.cs
@@ -5,10 +5,35 @@ using UnityEngine;
 public class Objectsprojectiles : MonoBehaviour
 {
     private Transform target;
-    private float speed = 70f;
-    private Vector3 pos;
-    private bool isLevitated = false;
+    private Rigidbody rigidbody;
+
+    [SerializeField] private float speed = 5f;
     [SerializeField] private float levitateTime = 3f;
+
+    private Vector3 pos;
+    private bool isLevitated = false, stopedLevitate = false;
+    
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null) return;
+        if (!isLevitated && !stopedLevitate)
+        {
+            StartCoroutine(Levitate());
+        }
+        else if (isLevitated && rigidbody.velocity.magnitude == 0 && Time.time <= 20f)
+        {
+            Throw();
+        }
+        else if (Time.time > 20f) DeLevitate();
+        Debug.Log(Time.time);
+    }
 
     public void Seek(Transform _target)
     {
@@ -16,23 +41,28 @@ public class Objectsprojectiles : MonoBehaviour
         pos = target.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    public IEnumerator Levitate()
     {
-        if (target == null) return;
-        //transform.position = Vector3.MoveTowards(transform.position, pos, speed * Time.deltaTime);
-        if(!isLevitated) StartCoroutine(Levitate());
-    }
-
-    IEnumerator Levitate()
-    {
+        rigidbody.useGravity = false;
         float startTime = Time.time;
-        while(Time.time < startTime + levitateTime)
+        stopedLevitate = true;
+        while (Time.time < startTime + levitateTime)
         {
             transform.Translate(new Vector3(0, 0.5f, 0) * Time.deltaTime);
-            isLevitated = true;
             yield return null;
         }
+        isLevitated = true;
     }
 
+    void DeLevitate()
+    {
+        rigidbody.useGravity = true;
+        //timeCount = Time.time;
+    }
+
+    void Throw()
+    {
+        Vector3 throwVector = new Vector3((pos.x - transform.position.x), 0, (pos.z - transform.position.z)).normalized * speed;
+        rigidbody.AddForce(throwVector, ForceMode.Impulse);
+    }
 }
